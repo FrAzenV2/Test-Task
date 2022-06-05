4d584b3d4663c03f7a07eebec1c1c3318cf7cf91
FrAzenV2 <frazenv2@ya.ru>
2022-06-05 10:27:28 +0300

    Add IScarable interface

---
+++ Assets/Scripts/Interfaces/ComponentInterfaces/IScarable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Interfaces.ComponentInterfaces
+{
+    public interface IScarable
+    {
+        Vector3 ScareEpicenter { get; set; }
+        float ScareAmount { get; set; }
+    }
+}
