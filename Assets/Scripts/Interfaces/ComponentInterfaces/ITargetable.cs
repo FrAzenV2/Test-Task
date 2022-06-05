4159765022ad02be22249e8553a70d9a60089c78
FrAzenV2 <frazenv2@ya.ru>
2022-06-05 10:29:44 +0300

    Add ITargetable Interface

---
+++ Assets/Scripts/Interfaces/ComponentInterfaces/ITargetable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Interfaces.ComponentInterfaces
+{
+    public interface ITargetable
+    {
+        void SetTargetPosition(Vector3 pos);
+        
+        Vector3 CurrentTargetPos { get; }
+        Vector3 DirectionToTargetPos { get; }
+    }
+}
